e4087673a45985be59019f2c509bfedd2ae53262
wadalz1998 <thanhlocnguyen1402@gmail.com>
2024-01-05 22:06:24 +0700

    home

--- customers/src/controller/index.js
+++ customers/src/controller/index.js
@@ -3,36 +3,44 @@ const api = new Api();
 function getEle(id) {
   return document.getElementById(id);
 }
-
+let data = [];
 function getListProduct() {
   // pending => show loader
-  getEle("loader").style.display = "block";
+
+  document.getElementById("loader").style.display = "block";
   const promise = api.fetchData();
 
   promise
     .then(function (result) {
-      console.log("result", result.data);
-      renderUI(result.data);
+      // console.log("result", result.data);
+      data = result.data;
+      // renderUI(result.data);
+      // lastrenderUI(result.data);
       //hide loader
+      onChangeOptionProduct();
       getEle("loader").style.display = "none";
     })
     .catch(function (error) {
-      console.log("error", error);
+      // console.log("error", error);
       getEle("loader").style.display = "none";
     });
 }
 
 getListProduct();
-
+function lastrenderUI(data) {
+  if (data.length > 0) {
+    let newData = onChangeOptionProduct();
+    renderUI(newData);
+  }
+}
 function renderUI(data) {
-  console.log(data);
   var content = "";
   for (var i = 0; i < data.length; i++) {
     const product = data[i];
     content += `
         <div class="col-12 col-md-6 col-lg-4">
             <div class="card cardPhone">
-              <img src="./../assets/img/${product.image}" class="card-img-top" alt="..." />
+              <img src="${product.img}" class="card-img-top" alt="..." />
               <div class="card-body">
                 <div class="d-flex justify-content-between">
                   <div>
@@ -52,8 +60,8 @@ function renderUI(data) {
                     <i class="fa fa-star"></i>
                   </div>
                   <div>
-                    <button class="btnPhone-shadow">
-                      <i class="fa fa-shopping-cart"></i> Buy Now
+                    <button class="btnPhone-shadow" onclick="handleCart('${product.id}')" >
+                      <i class="fa fa-shopping-cart""></i> Đặt hàng
                     </button>
                   </div>
                 </div>
@@ -64,4 +72,47 @@ function renderUI(data) {
   }
 
   document.getElementById("productList").innerHTML = content;
+}
+let newCart = [];
+function handleCart(id) {
+  if (data.length > 0) {
+    for (var i = 0; i < data.length; i++) {
+      const dataTemp = data[i];
+      if (dataTemp.id === id) {
+        newCart.push(dataTemp);
+        moreArry.push({ ...newCart, count });
+      }
+    }
+
+    setLocalStorage(newCart);
+    // return newCart;
+  }
+}
+function showCart() {}
+function setLocalStorage(data) {
+  // convert data JSON=>String
+  const dataString = JSON.stringify(data);
+  // lưu
+  localStorage.setItem("items", dataString);
+}
+
+function onChangeOptionProduct() {
+  const optionSelected = getEle("form-select").value;
+  if (data.length > 0) {
+    const cloneData = [...data];
+    let newArray = [];
+    if (optionSelected === "all") {
+      return renderUI(cloneData);
+    } else {
+      for (var i = 0; i < cloneData.length; i++) {
+        const newProduct = cloneData[i];
+        // console.log(newProduct);
+        if (newProduct.type === optionSelected) {
+          newArray.push(newProduct);
+        }
+      }
+    }
+    return renderUI(newArray);
+    // return newArray;
+  }
 }
--- customers/src/services/api.js
+++ customers/src/services/api.js
@@ -1,7 +1,7 @@
 function Api() {
   this.fetchData = function () {
     const promise = axios({
-      url: "https://6576fb1c197926adf62ceea5.mockapi.io/api/project",
+      url: "https://6576fb1c197926adf62ceeb1.mockapi.io/api/products",
       method: "GET",
     });
 
